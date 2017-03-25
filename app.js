11e998c8ca6ed015181a34765bf7bea7f2d05aba
Austin Lauritsen <austin@veryaustin.com>
2017-03-25 08:48:03 -0500

    Setup query string params to get a single book

--- app.js
+++ app.js
@@ -11,20 +11,32 @@ var Book = require('./models/bookModel');
 // Book Router
 var bookRouter = express.Router();
 
-// Create a new route '/books
+// Get Route for '/books
 bookRouter.route('/books')
   .get(function (req, res) {
     var query = {};
 
     if (req.query.genre) {
       query.genre = req.query.genre;
-    };
+    }
 
     Book.find(query, function (err, books) {
       if (err) {
         res.status(500).send(err);
       } else {
         res.json(books);
+      }
+    });
+  });
+
+// Get Route for single book
+bookRouter.route('/books/:bookId')
+  .get(function (req, res) {
+    Book.findById(req.params.bookId, function (err, book) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(book);
       }
     });
   });
