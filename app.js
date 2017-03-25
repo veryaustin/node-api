27bf9a49512a7fb9c932891c38f189998b57ec4f
Austin Lauritsen <austin@veryaustin.com>
2017-03-25 07:52:16 -0500

    Use comments to document app.js

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+// Require Express
 var express = require('express');
 
+// Instantiate an instance of express and assign it to app
 var app = express();
 
+// Set the port to the environment variable port or 3000 if it is not defined
 var port = process.env.PORT || 3000;
 
+// On a get request to / send 'welcome to the api' to the client
 app.get('/', function (req, res) {
   res.send('welcome to the API');
 });
 
+// Set the server to listen on the port
 app.listen(port, function () {
   console.log('Running on PORT: ' + port);
 });
