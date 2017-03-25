5a969af69754397035fef2b87d6b6172f6940b42
Austin Lauritsen <austin@veryaustin.com>
2017-03-25 09:53:49 -0500

    Implement Book PATCH

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -49,8 +49,28 @@ var routes = function (Book) {
       req.book.author = req.body.author;
       req.book.genre = req.body.genre;
       req.book.read = req.body.read;
-      req.book.save();
-      res.json(req.book);
+      req.book.save(function(err) {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.json(req.book);
+        }
+      });
+    })
+    .patch(function (req, res) {
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      for (var p in req.body) {
+        req.book[p] = req.body[p];
+      }
+      req.book.save(function (err) {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.json(req.book);
+        }
+      });
     });
   return bookRouter;
 };
